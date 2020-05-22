44bccc3496fb85090b494cf5cbb7b16b063132be
islamMaruf <marufislam7424@gmail.com>
2020-05-22 16:03:13 +0600

    redux store created

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CakeContainer from './components/CakeContainer';
+import {Provider} from 'react-redux'
+import store from './redux/store'
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}> 
+      <div className="App">
       <CakeContainer />
     </div>
+    </Provider>
+    
   );
 }
 
